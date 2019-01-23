51fa326d3dfd6288bdb6eabb2b0229090e951cf7
m3rt32 <mertkirimgeri@gmail.com>
2019-01-23 15:32:01 +0300

    Initial Source

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ChangeScene : MonoBehaviour
+{
+    float counter;
+    public Text _text;
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = ((int)counter).ToString();
+        counter += Time.deltaTime;
+        if (counter > 15f)
+        {
+            SceneManager.LoadScene("AR2");
+        }
+    }
+}
