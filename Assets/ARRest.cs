5e3241a52a8232fe14da75727d4c36bf9a032364
m3rt32 <mertkirimgeri@gmail.com>
2019-01-29 21:00:59 +0300

    AR Subsystem fix

---
+++ Assets/ARRest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ARRest : MonoBehaviour
+{
+    [SerializeField]
+    ARSession arSession;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARSubsystemManager.DestroySubsystems();
+        ARSubsystemManager.CreateSubsystems();
+        ARSubsystemManager.StopSubsystems();
+        arSession.gameObject.SetActive(false);
+        StartCoroutine(StartTracking());
+    }
+
+    IEnumerator  StartTracking()
+    {
+        yield return new WaitForSeconds(1f);
+        arSession.gameObject.SetActive(true);
+        ARSubsystemManager.StartSubsystems();
+      //  arPlaneManager.planeAdded += PlaneTracked;
+    }
+
+}
--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 public class ChangeScene : MonoBehaviour
 {
     float counter;
     public Text _text;
+    bool triggerOnce = true;
     // Update is called once per frame
     void Update()
     {
         _text.text = ((int)counter).ToString();
         counter += Time.deltaTime;
-        if (counter > 15f)
+        if (counter > 15f && triggerOnce)
         {
-            SceneManager.LoadScene("AR2");
+            StopAR();
+            triggerOnce = false;
         }
+    }
+    public void StopAR()
+    {
+        ARSubsystemManager.StopSubsystems();
+        GameObject arsession = GameObject.Find("ARSession");
+        Destroy(arsession);
+        Invoke("Change", 1f);
+    }
+    
+    private void Change()
+    {
+        SceneManager.LoadScene("AR2");
     }
 }
